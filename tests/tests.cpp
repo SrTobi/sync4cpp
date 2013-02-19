69f62e96cf5b0a851a604f1e8df8d4862551b334
SirTobi <Code.DataByte@gmail.com>
2013-02-20 00:55:59 +0100

    Small comment added.

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,2 +1,7 @@
+/************************************************
+ * 
+ *	This file only sets the test module.
+ *
+ ************************************************/
 #define BOOST_TEST_MODULE sync4cpp test
 #include <boost/test/unit_test.hpp>
