220e126ae7d46dc6152374fd30cf0b6bd5e0d345
SirTobi <code.databyte@googlemail.com>
2013-03-20 03:10:38 +0100

    fixed const parameter problems.

--- include/sync4cpp-guardarray.hpp
+++ include/sync4cpp-guardarray.hpp
@@ -20,6 +20,18 @@ namespace detail {
 		inline static unused_type& Inst() { static unused_type uused; return uused; }
 	};
 
+	template<typename Type>
+	struct make_assignment_const_ref
+	{
+		typedef Type type;
+	};
+
+	template<typename Mutex, typename Modifier>
+	struct make_assignment_const_ref<assignment<Mutex, Modifier> >
+	{
+		typedef const assignment<Mutex, Modifier> type;
+	};
+
 	static unused_type& UInst() { static unused_type uused; return uused; }
 
 	template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8>
@@ -27,8 +39,14 @@ namespace detail {
 	{
 		
 
-		GuardArray(	T1& m1, T2& m2 = UInst(), T3& m3 = UInst(), T4& m4 = UInst(),
-					T5& m5 = UInst(), T6& m6 = UInst(), T7& m7 = UInst(), T8& m8 = UInst())
+		GuardArray(	typename make_assignment_const_ref<T1>::type& m1, 
+					typename make_assignment_const_ref<T2>::type& m2 = UInst(),
+					typename make_assignment_const_ref<T3>::type& m3 = UInst(),
+					typename make_assignment_const_ref<T4>::type& m4 = UInst(),
+					typename make_assignment_const_ref<T5>::type& m5 = UInst(),
+					typename make_assignment_const_ref<T6>::type& m6 = UInst(),
+					typename make_assignment_const_ref<T7>::type& m7 = UInst(),
+					typename make_assignment_const_ref<T8>::type& m8 = UInst())
 			: guard1(m1)
 			, guard2(m2)
 			, guard3(m3)
@@ -57,7 +75,7 @@ namespace detail {
 
 	template<typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename Unused>
 	inline GuardArray<T1, T2, T3, T4, T5, T6, T7, T8>
-		GetGuardType(T1& p1, T2& p2, T3& p3, T4& p4, T5& p5, T6& p6, T7& p7, T8& p8, Unused&, ...)
+		GetGuardType(const T1& p1, const T2& p2, const T3& p3, const T4& p4, const T5& p5, const T6& p6, const T7& p7, const T8& p8, const Unused&, ...)
 	{
 		static_assert(std::is_same<Unused, unused_type>::value, "You can only synchronize up to 8 mutexes with a guard array!");
 		assert(!"Never call this!");
